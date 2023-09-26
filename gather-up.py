4f756818159cf1fed557bf8a3e4f62fd4ec8099c
Max Hirtenstein <maxhirtens@gmail.com>
2023-09-26 15:00:12 -0700

    finished basic functions

--- gather-up.py
+++ gather-up.py
@@ -1,23 +1,23 @@
 import os
 import sys
 import shutil
+import datetime
 
 # os methods
 abspath = os.path.abspath
 listdir = os.listdir
 
 # shutil methods
-move = shutil.move
+copy = shutil.copy2
 
 # sys.argv[1] is your source folder. Any photos inside subdirectories here will
 # be copied to a folder at the top level of this folder called '_Gathered-Files' on execution.
 
 # For example, 'python gather-up.py ~/Desktop/images' will search
-# through all subfolders of /images and move all files to the top level.
+# through all subfolders of /images and copy all files to a new top level folder.
 
 # If there are duplicate filenames, you will be prompted to overwrite or skip by the OS.
 
-# need to make it work for folders with a space in the name
 src_folder = abspath(sys.argv[1])
 
 print("Source Folder:" + src_folder)
@@ -33,20 +33,36 @@ filelist = []
 
 for root, dirs, files in os.walk(src_folder):
     for file in files:
+        # ignore .DS_Store files
+        if file.endswith(".DS_Store"):
+            print("Skipping " + file)
+            continue
         filelist.append(os.path.join(root, file))
+
+print(filelist)
 
 # Move all files to the new folder
 try:
     for file in filelist:
-        # move file if it doesn't start with a period
-        if file.endswith(".DS_Store"):
-            print("Skipping " + file)
-            continue
-        else:
-            move(file, gathered_files_folder)
-            print("Moved " + file)
+        # check for duplicate names and rename if necessary
+        # help from https://pythonguides.com/python-copy-file/
+        head_tail = os.path.split(file)
+        if os.path.exists(os.path.join(gathered_files_folder, head_tail[1])):
+            print(f"{file} is a duplicate, renaming...")
+            base_name = os.path.basename(file)
+            new_file_name = (
+                "duplicate_"
+                + datetime.datetime.now().strftime("%H-%M-%S")
+                + "_"
+                + base_name
+            )
+            copied_file = os.path.join(gathered_files_folder, base_name)
+            new_file = os.path.join(gathered_files_folder, new_file_name)
+            os.rename(copied_file, new_file)
+        copy(file, gathered_files_folder)
+        print("Copied " + file)
 
 except Exception as err:
-    print("Error moving some files: " + err)
+    print(err)
 
 print("Gathered up!")
